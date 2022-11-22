8bf4fb26fd5d994b3f926ef7160e13d38a3da8b0
Nate Potter <91092832+potternate@users.noreply.github.com>
2022-11-21 20:19:38 -0800

    Update tweet_artist_mentions.py
    Now can update status with media.

--- tweepy/tweet_artist_mentions.py
+++ tweepy/tweet_artist_mentions.py
@@ -5,12 +5,13 @@ import pandas as pd
 
 client = tweepy.Client(config.bearer_token, config.api_key, config.api_secret, config.access_token, config.access_token_secret)
 
-artist = 'A Boogie'
-project = 'single "Ballin"'
+artist = 'Lil Uzi Vert'
+project = 'music video "Just Wanna Rock"'
 query = artist + ' (-is:retweet -is:reply)'
+cover_art = "justwannarock_mv.jpeg"
 
-start_time = '2022-11-03T04:00:00Z'
-end_time = '2022-11-04T04:00:00Z'
+start_time = '2022-11-18T04:00:00Z'
+end_time = '2022-11-19T04:00:00Z'
 
 counts = client.get_recent_tweets_count(query=query, granularity='day', start_time=start_time, end_time=end_time)
 
@@ -21,4 +22,5 @@ for count in counts.data:
 auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth)
-api.update_status(artist + ' mentioned in ' + str(total) + ' tweets in 24 hours following release of ' + project)
+api.update_status_with_media(status = artist + ' mentioned in ' + str(total) + ' tweets in 24 hours following release of ' + project, filename = cover_art)
+
