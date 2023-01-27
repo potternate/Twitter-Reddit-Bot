4d0ad87ed71245c24c5c7f0753d1a4bbe8fd4cae
Nate Potter <91092832+potternate@users.noreply.github.com>
2023-01-26 17:37:15 -0800

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ logger = logging.getLogger()
 
 
 def create_api():
-    consumer_key = secrets.api_key
-    consumer_secret = secrets.api_secret
-    access_token = secrets.access_token
-    access_token_secret = secrets.access_token_secret
+    consumer_key = 'kjPmXkwmi6MVrYdOi6PtNSTxr'
+    consumer_secret = 'vRZKJ3QTLh58TWRdEIm8O4RAKOAxxh9OliwbxAdBmjW31iwsr3'
+    access_token = '773736831112339456-KMSYlbxp8k5oSONBc9i3XDzjzP8SRJM'
+    access_token_secret = 'jMlasQSOcvHYMV90sw7tbLo5fACrEYVeQUXHPWWa7tI95'
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
