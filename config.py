fd7b0056a0171f029da4ae2658fcf2a44cafeb87
Nate Potter <91092832+potternate@users.noreply.github.com>
2023-01-27 11:41:30 -0800

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ logger = logging.getLogger()
 
 
 def create_api():
-    api_key = os.getenv.TWITTER_API_KEY
-    api_key_secret = os.getenv.TWITTER_API_KEY_SECRET
-    access_token = os.getenv.TWITTER_ACCESS_TOKEN
-    access_token_secret = os.getenv.TWITTER_ACCESS_TOKEN_SECRET
+    api_key = os.getenv("TWITTER_API_KEY")
+    api_key_secret = os.getenv("TWITTER_API_KEY_SECRET")
+    access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
     auth = tweepy.OAuthHandler(api_key, api_key_secret)
     auth.set_access_token(access_token, access_token_secret)
