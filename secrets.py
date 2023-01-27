805d3817b95fde691736afd10dd4a70b56da6c4d
Nate Potter <91092832+potternate@users.noreply.github.com>
2023-01-26 17:18:40 -0800

    Delete secrets.py

--- secrets.py
+++
@@ -1,5 +0,0 @@
-api_key = 'kjPmXkwmi6MVrYdOi6PtNSTxr'
-api_secret = 'vRZKJ3QTLh58TWRdEIm8O4RAKOAxxh9OliwbxAdBmjW31iwsr3'
-bearer_token = r'AAAAAAAAAAAAAAAAAAAAALXlfwEAAAAAzkI5HHKRZdjJxy1Y65we71lwDCo%3DPTnkBy06bJE2btIncECZUX6Fwx1CgucN0O3YrGYpcyUBhZfsFi'
-access_token = '773736831112339456-KMSYlbxp8k5oSONBc9i3XDzjzP8SRJM'
-access_token_secret = 'jMlasQSOcvHYMV90sw7tbLo5fACrEYVeQUXHPWWa7tI95'
