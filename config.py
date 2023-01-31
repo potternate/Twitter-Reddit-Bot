e8a12f3d5ef2804f9fd057a58bdf49c1761d2174
Nate Potter <91092832+potternate@users.noreply.github.com>
2023-01-31 12:14:17 -0800

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,7 @@ def create_api():
 
     auth = tweepy.OAuthHandler(api_key, api_key_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True,
-                     wait_on_rate_limit_notify=True)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
     try:
         api.verify_credentials()
     except Exception as e:
