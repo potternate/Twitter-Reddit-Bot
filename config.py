8d42fcd71eabe72f193cd3d30428e7dd41782329
Nate Potter <91092832+potternate@users.noreply.github.com>
2023-01-31 11:37:41 -0800

    Update config.py

--- config.py
+++ config.py
@@ -21,3 +21,13 @@ def create_api():
         raise e
     logger.info("API created")
     return api
+
+def create_reddit_instance():
+    reddit = praw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+        username=os.getenv("REDDIT_USERNAME"),
+        password=os.getenv("REDDIT_PASSWORD"),
+        user_agent="praw_scraper_1.0",
+    )
+    return reddit
