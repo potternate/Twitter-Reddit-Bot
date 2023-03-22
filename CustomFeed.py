8c0a1713984e86ca64b28ecc4c420a0697338ab5
Nate Potter <91092832+potternate@users.noreply.github.com>
2023-03-21 21:39:17 -0700

    Create CustomFeed.py

---
+++ CustomFeed.py
@@ -0,0 +1,17 @@
+# Here you can enter a custom feed of subreddits. 
+
+subreddits = ['abandonedporn', 
+              'architectureporn', 
+              'artefactporn', 
+              'beamazed', 
+              'carporn', 
+              'cityporn', 
+              'damnthatsinteresting', 
+              'designporn', 
+              'earthporn',
+              'exposureporn', 
+              'historyporn', 
+              'interestingasfuck', 
+              'militaryporn', 
+              'oldschoolcool', 
+              'spaceporn']
